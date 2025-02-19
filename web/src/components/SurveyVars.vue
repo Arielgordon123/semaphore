8e3052cdb9cf96673a6bdd51432a8a369cbfc5b0
Ariel <arielgordon123@gmail.com>
2025-02-19 19:59:55 +0000

    fix: fix multi enum type

--- web/src/components/SurveyVars.vue
+++ web/src/components/SurveyVars.vue
@@ -47,7 +47,7 @@
             ></v-select>
 
             <v-data-table
-              v-if="editedVar.type === 'enum'"
+              v-if="editedVar.type === 'enum' || editedVar.type === 'multi_enum'"
               :items="editedValues"
               :items-per-page="-1"
               class="elevation-1"
@@ -92,15 +92,9 @@
               <v-btn
                 color="primary"
                 class="d-block"
-                v-if="editedVar.type === 'enum'"
+                v-if="editedVar.type === 'enum' || editedVar.type === 'multi_enum'"
                 @click="addEditedVarValue()"
               >Add Value</v-btn>
-
-              <v-checkbox
-                v-if="editedVar.type === 'enum'"
-                :label="$t('allowMultipleValues')"
-                v-model="editedVar.multiple"
-              />
             </div>
 
             <v-checkbox
@@ -193,6 +187,9 @@ export default {
       }, {
         id: 'enum',
         name: 'Enum',
+      }, {
+        id: 'multi_enum',
+        name: 'Multi Enum',
       }],
       formError: null,
     };
@@ -235,10 +232,7 @@ export default {
         return;
       }
 
-      if (this.editedVar.type === 'enum') {
-        if (this.editedVar.multiple) {
-          this.editedVar.type = 'multi_enum';
-        }
+      if (this.editedVar.type === 'enum' || this.editedVar.type === 'multi_enum') {
         if (this.editedValues.length === 0) {
           this.formError = 'Enumeration must have values.';
           return;
